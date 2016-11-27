2662467a6b19fa447a118b86726f0f6bef2a1f3d
Collin Kruger <collin.kruger@live.com>
2016-11-26 21:50:05 -0600

    Initial

---
+++ CK.System.ConsoleColor/ConsoleColorSet_Background.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CK.System.ConsoleColor
+{
+    using static global::CK.System.Disposable.Disposable;
+    using static global::System.Console;
+    using static global::System.ConsoleColor;
+
+    public static partial class ConsoleColorSet
+    {
+        /// <summary>
+        /// Sets the the background color of the <see cref="global::System.Console"/> and returns an <see cref="global::System.IDisposable"/> that when disposed reassigns the previous color.
+        /// </summary>
+        public static IDisposable Background(global::System.ConsoleColor color)
+        {
+            var old = BackgroundColor;
+
+            BackgroundColor = color;
+
+            return Gen(() =>
+            {
+                ForegroundColor = old;
+            });
+        }
+
+        public static IDisposable BackgroundBlack() => Foreground(Black);
+        public static IDisposable BackgroundDarkBlue() => Foreground(DarkBlue);
+        public static IDisposable BackgroundDarkGreen() => Foreground(DarkGreen);
+        public static IDisposable BackgroundDarkCyan() => Foreground(DarkCyan);
+        public static IDisposable BackgroundDarkRed() => Foreground(DarkRed);
+        public static IDisposable BackgroundDarkMagenta() => Foreground(DarkMagenta);
+        public static IDisposable BackgroundDarkYellow() => Foreground(DarkYellow);
+        public static IDisposable BackgroundGray() => Foreground(Gray);
+        public static IDisposable BackgroundDarkGray() => Foreground(DarkGray);
+        public static IDisposable BackgroundBlue() => Foreground(Blue);
+        public static IDisposable BackgroundGreen() => Foreground(Green);
+        public static IDisposable BackgroundCyan() => Foreground(Cyan);
+        public static IDisposable BackgroundRed() => Foreground(Red);
+        public static IDisposable BackgroundMagenta() => Foreground(Magenta);
+        public static IDisposable BackgroundYellow() => Foreground(Yellow);
+        public static IDisposable BackgroundWhite() => Foreground(White);
+    }
+}
---
+++ CK.System.ConsoleColor/ConsoleColorSet_Foreground.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CK.System.ConsoleColor
+{
+    using static global::CK.System.Disposable.Disposable;
+    using static global::System.Console;
+    using static global::System.ConsoleColor;
+
+    /// <summary>
+    /// Set/undo the <see cref="global::System.Console.ForegroundColor"/> and <see cref="global::System.Console.BackgroundColor"/>, leveraging <see cref="global::System.IDisposable"/>.
+    /// </summary>
+    public static partial class ConsoleColorSet
+    {
+        /// <summary>
+        /// Sets the the foreground color of the <see cref="global::System.Console"/> and returns an <see cref="global::System.IDisposable"/> that when disposed reassigns the previous color.
+        /// </summary>
+        public static IDisposable Foreground(global::System.ConsoleColor color)
+        {
+            var old = ForegroundColor;
+
+            ForegroundColor = color;
+
+            return Gen(() =>
+            {
+                ForegroundColor = old;
+            });
+        }
+        
+        public static IDisposable ForegroundBlack() => Foreground(Black);
+        public static IDisposable ForegroundDarkBlue() => Foreground(DarkBlue);
+        public static IDisposable ForegroundDarkGreen() => Foreground(DarkGreen);
+        public static IDisposable ForegroundDarkCyan() => Foreground(DarkCyan);
+        public static IDisposable ForegroundDarkRed() => Foreground(DarkRed);
+        public static IDisposable ForegroundDarkMagenta() => Foreground(DarkMagenta);
+        public static IDisposable ForegroundDarkYellow() => Foreground(DarkYellow);
+        public static IDisposable ForegroundGray() => Foreground(Gray);
+        public static IDisposable ForegroundDarkGray() => Foreground(DarkGray);
+        public static IDisposable ForegroundBlue() => Foreground(Blue);
+        public static IDisposable ForegroundGreen() => Foreground(Green);
+        public static IDisposable ForegroundCyan() => Foreground(Cyan);
+        public static IDisposable ForegroundRed() => Foreground(Red);
+        public static IDisposable ForegroundMagenta() => Foreground(Magenta);
+        public static IDisposable ForegroundYellow() => Foreground(Yellow);
+        public static IDisposable ForegroundWhite() => Foreground(White);
+    }
+}
